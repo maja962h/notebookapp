49a839513e8f9887abc33e0799b7efade95f5db8
majamatboue <maja962h@stud.kea.dk>
2023-02-07 08:40:01 +0100

    Initial Commit

---
+++ NotebookDemo/NotebookDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotebookDemoApp.swift
+//  NotebookDemo
+//
+//  Created by Maja Matboue on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotebookDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
